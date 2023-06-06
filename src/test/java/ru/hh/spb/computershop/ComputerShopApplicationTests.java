d7a348e8756c0f1e320b3b1357fed66bc5451fd5
5h15h4k1n9 <st077017@studetn.spbu.ru>
2023-06-06 18:52:17 +0300

    Initialized Spring project

---
+++ src/test/java/ru/hh/spb/computershop/ComputerShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hh.spb.computershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
