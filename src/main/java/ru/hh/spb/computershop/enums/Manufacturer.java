f0a564fd27200e2727da9583b140a22e0d0b064d
5h15h4k1n9 <st077017@studetn.spbu.ru>
2023-06-07 01:46:53 +0300

    Added manufacturers

---
+++ src/main/java/ru/hh/spb/computershop/enums/Manufacturer.java
@@ -0,0 +1,16 @@
+package ru.hh.spb.computershop.enums;
+
+public enum Manufacturer implements java.io.Serializable {
+
+    ASUS,
+    ACER,
+    APPLE,
+    SAMSUNG,
+    WESTERN_DIGITAL,
+    SEAGATE,
+    TOSHIBA,
+    AOC,
+    LG,
+    SONY,
+    MICROSOFT,
+}
