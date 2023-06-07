68effb0081cfd6c55ba54dabe207777c8f941a01
5h15h4k1n9 <st077017@studetn.spbu.ru>
2023-06-07 03:12:01 +0300

    Refactored application class

--- src/main/java/ru/hh/spb/computershop/ComputerShopApplication.java
+++ src/main/java/ru/hh/spb/computershop/ComputerShopApplication.java
@@ -2,8 +2,10 @@ package ru.hh.spb.computershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories({"ru.hh.spb.computershop.repositories"})
 public class ComputerShopApplication {
 
     public static void main(String[] args) {
