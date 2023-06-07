40bebd1f035e36a2f82d6402e81ef4f0f2727243
5h15h4k1n9 <st077017@studetn.spbu.ru>
2023-06-07 05:34:54 +0300

    Refactored error response record

--- src/main/java/ru/hh/spb/computershop/responses/ErrorResponse.java
+++ src/main/java/ru/hh/spb/computershop/responses/ErrorResponse.java
@@ -2,5 +2,5 @@ package ru.hh.spb.computershop.responses;
 
 import org.springframework.http.HttpStatus;
 
-public record ErrorResponse(String message, HttpStatus status, String timestamp) {
+public record ErrorResponse(String message, HttpStatus status, String timestamp) implements ShopResponse {
 }
