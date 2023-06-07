5ba71c86b062cd4c6597d02d6681e8d37a3c8217
5h15h4k1n9 <st077017@studetn.spbu.ru>
2023-06-07 10:02:12 +0300

    Documented health controller

--- src/main/java/ru/hh/spb/computershop/controllers/HealthController.java
+++ src/main/java/ru/hh/spb/computershop/controllers/HealthController.java
@@ -1,7 +1,10 @@
 package ru.hh.spb.computershop.controllers;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +16,16 @@ public class HealthController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @GetMapping("")
+    @ApiOperation(
+            value = "Health check",
+            notes = "Returns OK if service is alive"
+    )
+    @ApiResponse(code = 200, message = "OK",
+            examples = @io.swagger.annotations.Example(
+                    value = {@io.swagger.annotations.ExampleProperty(value = "OK", mediaType = MediaType.TEXT_PLAIN_VALUE)}
+            )
+    )
+    @GetMapping(value = "", produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> health() {
         logger.info("Health check");
         return ResponseEntity.ok("OK");
